c527372a1db84af1f4f76752b8b69b54463284ac
dylan-gregory <jdylangregory@gmail.com>
2017-05-03 16:20:36 -0400

    Added a link to purchase each album

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
 
   return (
     <Card>
@@ -27,6 +28,12 @@ const AlbumDetail = ({ album }) => {
           style={styles.imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+        Buy Now
+        </Button>
       </CardSection>
 
     </Card>
